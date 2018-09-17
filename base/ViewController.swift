40226fce2f4ef4aa6e8195a00524f9014218b0a2
Guilherme Ramos <guilherme.hor@gmail.com>
2018-09-16 22:50:39 -0300

    Initial Commit

---
+++ base/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  base
+//
+//  Created by Guilherme on 9/16/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
